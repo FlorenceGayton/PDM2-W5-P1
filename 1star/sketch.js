3277fada1c8230b8b31c7f88103b9fc2cfaff2e8
Flo :) <103675778+FlorenceGayton@users.noreply.github.com>
2026-03-09 14:23:55 +0000

    Refactor sketch.js for improved layout and functionality
    Refactor setup and draw functions to use a main container for layout. Update button creation and event handling for animal images.

--- 1star/sketch.js
+++ 1star/sketch.js
@@ -1,28 +1,58 @@
-let bear;
-let cat;
-let monkey;
 let img;
+let bearButton;
+let catButton;
+let monkeyButton;
 
-function setup(){
-    createCanvas(600, 600);
-    // bear
-    bear = createButton("bear");
-    bear.position(200, 200);
-    // cat
-    cat = createButton("cat");
-    cat.position(300, 200);
-    // monkey
-    monkey = createButton("monkey");
-    monkey.position(400, 200);
-
-}
-
-function draw(){
-    image(img, 0, 0);
-
-}
-
+// declare container
+let mainContainer;
 
 function getAnimalImage(name){
     img = loadImage("assets/" + name + ".jpg");
+}
+
+function setup() {
+    mainContainer = createDiv();
+    mainContainer.style("width", "600px");
+    mainContainer.style("margin", "auto");
+    mainContainer.style("text-align", "center");
+
+    let canvas = createCanvas(600, 400);
+    canvas.parent(mainContainer);
+
+    // create buttons
+    bearButton = createButton("bear");
+    bearButton.parent(mainContainer);
+
+    catButton = createButton("cat");
+    catButton.parent(mainContainer);
+
+    monkeyButton = createButton("monkey");
+    monkeyButton.parent(mainContainer);
+
+
+    // show bear
+    bearButton.mousePressed(function() {
+        getAnimalImage("bear");
+    });
+    // show cat
+    catButton.mousePressed(function() {
+        getAnimalImage("cat");
+    });
+    // show monkey
+    monkeyButton.mousePressed(function() {
+        getAnimalImage("monkey");
+    });
+}
+
+function draw() {
+  background(157, 170, 210);
+  // only show image if one has been loaded
+  if (img !== undefined) {
+    image(img, 0, 0, width, height);
+  }
+}
+
+//load the image
+function getAnimalImage(name) {
+  img = loadImage("assets/" + name + ".jpg");
 }
